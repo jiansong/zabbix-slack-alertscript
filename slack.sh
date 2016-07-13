82ac60fedbd8c5fd1a78050c8751c67e22ac4b6e
Jiansong Liu <jiansong.liu@gmail.com>
2016-07-13 09:50:24 +0200

    Add 4th arg to indicate the severity, and put text inside attachments

--- slack.sh
+++ slack.sh
@@ -4,7 +4,7 @@
 url='CHANGEME'		# example: https://hooks.slack.com/services/QW3R7Y/D34DC0D3/BCADFGabcDEF123
 username='Zabbix'
 
-## Values received by this script:
+# Values received by this script:
 # To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
 # Subject = $2 (usually either PROBLEM or RECOVERY)
 # Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
@@ -12,21 +12,47 @@ username='Zabbix'
 # Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY)
 to="$1"
 subject="$2"
+message="$3"
+severity="$4"
 
 # Change message emoji depending on the subject - smile (RECOVERY), frowning (PROBLEM), or ghost (for everything else)
-recoversub='^RECOVER(Y|ED)?$'
-if [[ "$subject" =~ ${recoversub} ]]; then
-	emoji=':smile:'
-elif [ "$subject" == 'PROBLEM' ]; then
-	emoji=':frowning:'
-else
-	emoji=':ghost:'
+#recoversub='^RECOVER(Y|ED)?$'
+#if [[ "$subject" =~ ${recoversub} ]]; then
+#	emoji=':smile:'
+#elif [ "$subject" == 'PROBLEM' ]; then
+#	emoji=':frowning:'
+#else
+#	emoji=':ghost:'
+#fi
+
+if [ $subject == "OK" ]; then
+        color="good"
+else  
+    case "$severity" in 
+        "Not classified" )
+                color="#97AAB3"
+                ;;
+        "Information" )
+                color="#7499FF"
+                ;;
+        "Warning" )
+                color="#FFC859"
+                ;;
+        "Average" )
+                color="#FFA059"
+                ;;
+        "High" )
+                color="#E97659"
+                ;;
+        "Disaster" )
+                color="danger"
+                ;;
+        * )
+                color="#CCCCCC"
+                ;;
+    esac
 fi
 
-# The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
-#  followed by the message that Zabbix actually sent us ($3)
-message="${subject}: $3"
-
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
-payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
+payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"attachments\": [ { \"fallback\":\"${message//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"color\": \"${color//\"/\\\"}\" } ] }"
 curl -m 5 --data-urlencode "${payload}" $url -A 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
